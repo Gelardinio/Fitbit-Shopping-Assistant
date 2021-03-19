c3e567f2c638802a1a76de3f393f54b2670e6e48
Gerald Liu <geraldzliu@outlook.com>
2021-03-18 21:49:15 -0400

    First Push from Fitbit SDK

---
+++ app/index.js
@@ -0,0 +1,9 @@
+/*
+ * Entry point for the watch app
+ */
+import document from "document";
+
+let demotext = document.getElementById("demotext");
+demotext.x = 168;
+demotext.y = 50;
+demotext.text = "";
