56ee85dc9fb99ffbdb1fee2a878bbd183a121c42
Gerald Liu <geraldzliu@outlook.com>
2021-04-25 21:22:17 -0400

    Minor commit

--- app/index.js
+++ app/index.js
@@ -5,9 +5,11 @@ import document from "document";
 
 let screen1 = document.getElementById("screen1");
 let screen2 = document.getElementById("screen2");
+let screen3 = document.getElementById("screen3");
 
 const button1 = document.getElementById("button-1");
 const button2 = document.getElementById("button-2");
+const button4 = document.getElementById("button-4");
 
 let demotext = document.getElementById("demotext");
 demotext.x = 168;
@@ -18,14 +20,12 @@ function showScreen1() {
   console.log("Show screen 1");
   screen1.style.display = "inline"; 
   screen2.style.display = "none";
-  screen3.style.display = "none";
 }
 
 function showScreen2() {
   console.log("Show screen 2");
   screen1.style.display = "none"; 
   screen2.style.display = "inline";
-  screen3.style.display = "none";
 }
 
 button1.addEventListener("click", (evt) => {
@@ -33,7 +33,7 @@ button1.addEventListener("click", (evt) => {
   showScreen2();
 })
 
-button2addEventListener("click", (evt) => {
+button4.addEventListener("click", (evt) => {
   console.log("CLICKED 2");
   showScreen1();
 })
