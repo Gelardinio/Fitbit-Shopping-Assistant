c18a5278152b3cc6d302f1bd4c57462d424b607a
Gerald Liu <geraldzliu@outlook.com>
2021-04-25 21:03:52 -0400

    Migrated newer app version over

--- app/index.js
+++ app/index.js
@@ -3,7 +3,37 @@
  */
 import document from "document";
 
+let screen1 = document.getElementById("screen1");
+let screen2 = document.getElementById("screen2");
+
+const button1 = document.getElementById("button-1");
+const button2 = document.getElementById("button-2");
+
 let demotext = document.getElementById("demotext");
 demotext.x = 168;
 demotext.y = 50;
 demotext.text = "";
+
+function showScreen1() {
+  console.log("Show screen 1");
+  screen1.style.display = "inline"; 
+  screen2.style.display = "none";
+  screen3.style.display = "none";
+}
+
+function showScreen2() {
+  console.log("Show screen 2");
+  screen1.style.display = "none"; 
+  screen2.style.display = "inline";
+  screen3.style.display = "none";
+}
+
+button1.addEventListener("click", (evt) => {
+  console.log("CLICKED");
+  showScreen2();
+})
+
+button2addEventListener("click", (evt) => {
+  console.log("CLICKED 2");
+  showScreen1();
+})
