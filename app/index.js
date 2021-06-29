15c4c87c9cab12fca665385ae7217b3ff8ddb08b
Gerald <29520085+Gelardinio@users.noreply.github.com>
2021-06-29 16:10:41 -0400

    Final commit. App is done

--- app/index.js
+++ app/index.js
@@ -1,5 +1,5 @@
 /*
- * Entry point for the watch app
+ * By Gerald Liu - Fitbit Shopping Assistant
  */
 import document from "document";
 import * as messaging from "messaging";
@@ -8,11 +8,7 @@ let screen1 = document.getElementById("screen1");
 let screen2 = document.getElementById("screen2");
 let screen3 = document.getElementById("screen3");
 let screen4 = document.getElementById("screen4");
-let adidas1 = document.getElementById("adidas1");
-let adidas2 = document.getElementById("adidas2");
-let adidas3 = document.getElementById("adidas3");
-let incrementer = 1;
-let adidas = document.getElementById("adidas" + incrementer);
+let screen5 = document.getElementById("screen5");
 
 const button1 = document.getElementById("button-1");
 const button2 = document.getElementById("button-2");
@@ -21,12 +17,8 @@ const button4 = document.getElementById("button-4");
 const button5 = document.getElementById("button-5");
 const button6 = document.getElementById("button-6");
 const button7 = document.getElementById("button-7");
+const button8 = document.getElementById("button-8");
 let background = document.getElementById("background");
-
-let shirtLength = 0;
-let chestLength = 0;
-let waistLength = 0;
-let hipLength = 0;
 
 let User = class {
   constructor(gender, chestLength, waistLength, hipLength, inseamLength) {
@@ -100,7 +92,6 @@ messaging.peerSocket.onmessage = evt => {
   }
   if (evt.data.key === "inseam" && evt.data.newValue) {
     let inseam = JSON.parse(evt.data.newValue);
-    console.log("inseam: " + inseam);
     NewUser.editIL(inseam);
     changeMeasurements(NewUser);
     addTextS(NewUser, dictS);
@@ -119,17 +110,12 @@ messaging.peerSocket.onclose = () => {
 function changeMeasurements(userName) {
   if (userName.gender == "Male") {
     dictS = {adidasS: [35.5, 31, 36, 38.5, 33.5, 38.5, 42, 37.5, 42], nikeS: [36.25, 36.25, 31.5, 39.25, 39.25, 33.5, 42.5, 42.5, 36.5], pumaS: [36, 32.5, 37.5, 39.5, 35, 40.5, 42.6, 37.8, 43.5], uaS: [35, 29, 34.5, 39, 32, 37.5, 43, 36, 41], columbiaS: [35, 38.5, 37.5, 37, 30.5, 39.5, 40, 33, 42]};
-    dictP = {adidasP: [31, 36, 32.1, 33.5, 38.5, 32.3, 37, 42, 32.5]};
+    dictP = {adidasP: [31, 36, 32.1, 33.5, 38.5, 32.3, 37, 42, 32.5], nikeP: [30.5, 36, 32, 33.5, 39, 32.5, 36.5, 42.5, 33], pumaP: [31, 36, 32, 33, 38, 33, 36, 43, 33], uaP: [29, 35, 31, 32.5, 38, 32, 36, 41, 33], columbiaP: [30.5, 36, 30, 33.5, 39, 32, 37.5, 42, 34]};
   } else {
     dictS = {adidasS: [34, 27.6, 37.5, 36.5, 30, 40, 39, 33, 42.5], nikeS: [34, 27.5, 37, 36.5, 30.5, 40, 39.5, 33, 42.5], pumaS: [33.5, 27, 38, 36, 29, 40, 38, 31.5, 42], uaS: [34.5, 26.5, 36.5, 37, 29, 39, 39.5, 31.5, 41.5], columbiaS: [34.5, 28, 37, 37, 30, 39, 40, 32.5, 42]};
-    dictP = {adidasP: [38.5, 38, 30.5, 31.75, 40.5, 31, 34.5, 43.5, 31.5]};
+    dictP = {adidasP: [27.5, 37, 31, 30, 39.5, 31.5, 32.5, 42, 31.5], nikeP: [27.5, 37, 31, 30, 40, 31.5, 33, 42.5, 31.5], pumaP: [26, 37, 31, 29, 39, 31, 31, 41, 31.5], uaP: [26.5, 36.5, 30.5, 31.5, 39, 31, 31.5, 41.5, 32], columbiaP: [28, 37, 39.5, 39, 39, 32, 33, 42, 34.5]};
   }
 }
-
-//changeMeasurements(NewUser);
-
-//addTextS(NewUser, dictS);
-//addTextP(NewUser, dictP);
 
 function showScreen1() {
   console.log("Show screen 1");
@@ -167,6 +153,18 @@ function showScreen4() {
   screen4.style.display = "inline";
 }
 
+function showScreen5() {
+  console.log("Show screen 5");
+  screen1.style.display = "none"; 
+  screen5.style.display = "inline";
+}
+
+function showScreen14() {
+  console.log("Show screen 1");
+  screen5.style.display = "none";
+  screen1.style.display = "inline"; 
+}
+
 button1.addEventListener("click", (evt) => {
   console.log("CLICKED");
   showScreen2();
@@ -184,7 +182,7 @@ button3.addEventListener("click", (evt) => {
 
 button4.addEventListener("click", (evt) => {
   console.log("CLICKED");
-  showScreen4();
+  showScreen5();
 })
 
 button5.addEventListener("click", (evt) => {
@@ -202,14 +200,16 @@ button7.addEventListener("click", (evt) => {
   showScreen13();
 })
 
+button8.addEventListener("click", (evt) => {
+  console.log("CLICKED 3");
+  showScreen14();
+})
+
 function addTextS(nUser, dict) {
   for (let i in dict) {
       let range1 = Math.abs(nUser.chestLength - dict[i][0]) + Math.abs(nUser.waistLength - dict[i][1]) + Math.abs(nUser.hipLength - dict[i][2]);
       let range2 = Math.abs(nUser.chestLength - dict[i][3]) + Math.abs(nUser.waistLength - dict[i][4]) + Math.abs(nUser.hipLength - dict[i][5]);
       let range3 = Math.abs(nUser.chestLength - dict[i][6]) + Math.abs(nUser.waistLength - dict[i][7]) + Math.abs(nUser.hipLength - dict[i][8]);
-      console.log("Range 1: " + range1);
-      console.log("Range 2: " + range2);
-      console.log("Range 3: " + range3);
       for (let k = 0; k < 3; k++) {
         let temps = document.getElementById(i + (k + 1));
         temps.style.fill = "black";
@@ -238,9 +238,6 @@ function addTextP(nUser, dict) {
       let range1 = Math.abs(nUser.waistLength - dict[i][0]) + Math.abs(nUser.hipLength - dict[i][1]) + Math.abs(nUser.inseamLength  - dict[i][2]);
       let range2 = Math.abs(nUser.waistLength - dict[i][3]) + Math.abs(nUser.hipLength - dict[i][4]) + Math.abs(nUser.inseamLength - dict[i][5]);
       let range3 = Math.abs(nUser.waistLength - dict[i][6]) + Math.abs(nUser.hipLength - dict[i][7]) + Math.abs(nUser.inseamLength - dict[i][8]);
-      console.log("Range 1P: " + range1);
-      console.log("Range 2P: " + range2);
-      console.log("Range 3P: " + range3);
       for (let k = 0; k < 3; k++) {
         let temps = document.getElementById(i + (k + 1));
         temps.style.fill = "black";
@@ -266,6 +263,5 @@ function addTextP(nUser, dict) {
 
 function turnGreen(toFill) {
   toFill.style.fill = "green";
-  console.log("turn green");
 }
 
